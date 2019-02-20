e7e4a23767c91369f6eb560660987a8b9c938d2f
Nothing <mr.tianj.zh@gmail.com>
2019-02-20 14:21:33 +0800

    update

--- sync-fork.sh
+++ sync-fork.sh
@@ -1,13 +1,21 @@
 #!/bin/sh
-project=$1
-if [ ! -d "$project" ]; then
-  git clone https://github.com/tianj-zh/$project.git
-  pushd $project
-  git remote add upstream https://github.com/qt/$project.git
+local_path=$1
+project=$2
+origin_url=$3
+upstream_url=$4
+if [ ! -d "${local_path}" ]; then
+  mkdir "${local_path}"
+fi
+pushd ${local_path}
+if [ ! -d "${project}" ]; then
+  git clone ${origin_url}/${project}.git
+  pushd ${project}
+  git remote add upstream ${upstream_url}/${project}.git
   popd
 fi
-pushd $project
+pushd ${project}
 git fetch upstream
 git fetch origin
-for branch in $(git ls-remote --heads upstream|sed 's#^.*refs/heads/##'); do git push -f origin refs/remotes/upstream/$branch:refs/heads/$branch --tags; done
+for branch in $(git ls-remote --heads upstream|sed 's#^.*refs/heads/##'); do git push -f origin refs/remotes/upstream/${branch}:refs/heads/${branch} --tags; done
+popd
 popd
